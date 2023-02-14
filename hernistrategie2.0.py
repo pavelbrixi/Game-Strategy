52134a583f6fb5275c1e4df27d224c1b90c8b1a6
pavelbrixi <mortal.brixi@gmail.com>
2023-02-14 12:48:47 +0100

    Version 2.0

---
+++ hernistrategie2.0.py
@@ -0,0 +1,34 @@
+
+class Vojak:
+	def __init__(self):
+		self.pozice = 0
+		self.maximalnivitalita = 100
+		self.vitalita = 100
+		self.zasah = 20
+		self.grafika_1 = "   O    "
+		self.grafika_2 = "  /:\   "
+		self.grafika_3 = "  / \   "
+
+class Tank:
+	def __init__(self):
+		self.pozice = 0
+		self.maximalnivitalita = 200
+		self.vitalita = 200
+		self.zasah = 40	
+		self.grafika_1 = " _/-\==="
+		self.grafika_2 = ":     \ "
+		self.grafika_3 = "\O-O-O/ "
+		
+class Vozidlo:
+	def __init__(self):
+		self.pozice = 0
+		self.maximalnivitalita = 500
+		self.vitalita = 500
+		self.zasah = 70	
+		self.grafika_1 = ".---._  "
+		self.grafika_2 = ":   : \ "
+		self.grafika_3 = "´O-O-O/ "
+
+class Tym:
+	def __init__(self):
+		self
